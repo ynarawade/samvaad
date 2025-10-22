5e0b2de5876158f0f5c2955876993d733853b26a
ynarawade <yadnesh.narawade22@gmail.com>
2025-10-22 20:44:04 +0530

    implemented landing page

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { HeroHeader } from "./_components/header";
+import HeroSection from "./_components/hero-section";
+
+function LandingPage() {
+  return (
+    <>
+      <HeroHeader />
+      <HeroSection />
+    </>
+  );
+}
+
+export default LandingPage;
